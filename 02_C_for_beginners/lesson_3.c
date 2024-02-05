ffe3ef42823972dbbf61dd0b730568a5e807ba9b
Valery Somov <valerasomov@yahoo.com>
2024-02-05 23:37:39 +0300

    02_study-lesson_3

--- 02_C_for_beginners/lesson_3.c
+++ 02_C_for_beginners/lesson_3.c
@@ -8,11 +8,73 @@
 //     return 0;
 // }
 
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(void){
-    int round, total;
-    scanf("%d %d", &round, &total);
-    printf("%d %d", total/round, total - round*(total/round));
+// int main(void){
+//     int round, total;
+//     scanf("%d %d", &round, &total);
+//     printf("%d %d", total/round, total - round*(total/round));
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(void){
+//     double a, b;
+//     scanf("%lf %lf", &a, &b);
+//     printf("%.1lf", a*b);
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(void){
+//     double s, w, ch;
+//     scanf("%lf %lf %lf", &s, &w, &ch);
+//     printf("%.lf",(s/w)/ch);
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(void){
+//     int rub, kop, n=0, rest;
+//     scanf("%d %d", &rub, &kop);
+//     while ((n*100+n*5+n*1)<=(rub*100+kop))
+//     {
+//         n+=1;
+//     }
+//     printf("%d %d", n-1, rub*100+kop - (n-1)*106);
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(void){
+//     int n, day;
+//     scanf("%d %d", &n, &day);
+//     printf("%d", 1+(n-day)/7);
+//     return 0;
+// }
+
+#include <stdio.h>
+int main(void)
+{
+    int n;
+    double k;
+    scanf("%d %lf", &n, &k);
+    printf("%lf", n - k * n);
     return 0;
 }
+
+// #include <stdio.h>
+// int main(void){
+//     int n, day;
+//     scanf("%d %d", &n, &day);
+//     printf("%d", 1+(n-day)/7);
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(void){
+//     int n, day;
+//     scanf("%d %d", &n, &day);
+//     printf("%d", 1+(n-day)/7);
+//     return 0;
+// }
