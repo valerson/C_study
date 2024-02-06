d1c18041707efe26f982a9eb3d3d62aefd529516
Valery Somov <valerasomov@yahoo.com>
2024-02-06 11:43:52 +0300

    закончил урок цифры

--- 02_C_for_beginners/lesson_3.c
+++ 02_C_for_beginners/lesson_3.c
@@ -53,28 +53,38 @@
 //     return 0;
 // }
 
-#include <stdio.h>
-int main(void)
-{
-    int n;
-    double k;
-    scanf("%d %lf", &n, &k);
-    printf("%lf", n - k * n);
-    return 0;
-}
-
 // #include <stdio.h>
-// int main(void){
-//     int n, day;
-//     scanf("%d %d", &n, &day);
-//     printf("%d", 1+(n-day)/7);
+// int main(void)
+// {
+//     int n, res;
+//     double k;
+//     scanf("%d %lf", &n, &k);
+//     res=n - k * n;
+//     printf("%d", res);
 //     return 0;
 // }
 
 // #include <stdio.h>
 // int main(void){
-//     int n, day;
-//     scanf("%d %d", &n, &day);
-//     printf("%d", 1+(n-day)/7);
+//     int bell,nbell, n;
+//     scanf("%d %d", &bell, &nbell);
+//     n=nbell-bell+1;
+//     printf("%d", (bell+nbell)*n/2);
+//     return 0;
+// }
+
+// #include <stdio.h>
+// int main(void){
+//     int g_1, s_1, k_1, g_2, s_2, k_2;
+//     double total, g_res, s_res, k_res;
+//     scanf("%d %d %d", &g_1, &s_1, &k_1);
+//     scanf("%d %d %d", &g_2, &s_2, &k_2);
+//     total=g_1*17*29 + s_1*29 + k_1 + g_2*17*29 + s_2*29 + k_2;
+//     g_res = total/17/29;
+//     g_res=(int)g_res;
+//     s_res = (total-(int)g_res*17*29)/29;
+//     s_res=(int)s_res;
+//     k_res = total-(int)g_res*17*29-(int)s_res*29;
+//     printf("%.lf %.lf %.lf", g_res, s_res, k_res);
 //     return 0;
 // }
--- 02_C_for_beginners/tempCodeRunnerFile.c
+++ 02_C_for_beginners/tempCodeRunnerFile.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-int main(void)
-{
-    printf("hello world\n");
+int main(void){
+    int g_1, s_1, k_1, g_2, s_2, k_2;
+    double total, g_res, s_res, k_res;
+    scanf("%d %d %d", &g_1, &s_1, &k_1);
+    scanf("%d %d %d", &g_2, &s_2, &k_2);
+    total=g_1*17*29 + s_1*29 + k_1 + g_2*17*29 + s_2*29 + k_2;
+    g_res = total/17/29;
+    s_res = (total-(int)g_res*17*29)/29;
+    k_res = total-(int)g_res*17*29-(int)s_res*29;
+    printf("%.lf %.lf %.lf", g_res, s_res, k_res);
     return 0;
 }
